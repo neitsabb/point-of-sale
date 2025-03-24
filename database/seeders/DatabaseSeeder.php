468d3be89ae848b71e9ae0836026b982acbd4aa6
BastienVdp <bastienpp@hotmail.com>
2025-03-24 19:32:41 +0100

    create realistic products

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,44 +22,146 @@ class DatabaseSeeder extends Seeder
             'password' => 'password',
         ]);
 
-        $categories = ['Burgers', 'Boissons', 'Frites', 'Sauces', 'Viandes'];
+        $categories = collect([
+            'Burgers',
+            'Boissons',
+            'Frites',
+            'Sauces',
+            'Viandes'
+        ])->mapWithKeys(fn($categoryName) => [
+            $categoryName => Category::firstOrCreate(['name' => $categoryName])
+        ]);
 
-        foreach ($categories as $categoryName) {
-            Category::factory()->create([
-                'name' => $categoryName,
+        $ingredients = collect([
+            // Base
+            'Pain burger' => ['price' => 0.50, 'unit' => 'unit'],
+            'Pain Bicky' => ['price' => 0.60, 'unit' => 'unit'],
+
+            // Fromages
+            'Cheddar' => ['price' => 0.80, 'unit' => 'unit'],
+            'Gouda' => ['price' => 0.70, 'unit' => 'unit'],
+
+            // Légumes
+            'Salade' => ['price' => 0.03, 'unit' => 'g'],
+            'Oignons frits' => ['price' => 0.02, 'unit' => 'g'],
+            'Cornichons' => ['price' => 0.04, 'unit' => 'g'],
+
+            // Viandes
+            'Viande Classic' => ['price' => 0.03, 'unit' => 'g'], // 3€ les 100g
+            'Viande Bicky' => ['price' => 0.025, 'unit' => 'g'], // 2.50€ les 100g
+            'Viande Poulet' => ['price' => 0.028, 'unit' => 'g'], // 2.80€ les 100g
+            'Bacon' => ['price' => 0.05, 'unit' => 'g'],
+
+            // Sauces
+            'Sauce Bicky' => ['price' => 0.25, 'unit' => 'ml'],
+            'Sauce Poivre' => ['price' => 0.20, 'unit' => 'ml'],
+        ])->map(function ($data, $name) {
+            return Ingredient::firstOrCreate([
+                'name' => $name,
+                'price' => $data['price'],
+                'unit' => $data['unit'],
+                'stock_quantity' => rand(1000, 5000), // Quantités plus réalistes
+                'critical_stock' => rand(500, 1000)
             ]);
-        }
-
-        $categories = Category::all();
-
-        $products = Product::factory(50)->create()->each(function ($product) use ($categories) {
-            $product->category()->associate($categories->random())->save();
         });
 
-        $ingredients = Ingredient::factory(20)->create();
+        $products = collect([
+            // Burgers
+            [
+                'category' => 'Burgers',
+                'name' => 'Burger Bicky',
+                'tax' => 10,
+                'ingredients' => [
+                    ['name' => 'Pain Bicky', 'quantity' => 1],
+                    ['name' => 'Viande Bicky', 'quantity' => 150],
+                    ['name' => 'Sauce Bicky', 'quantity' => 20],
+                    ['name' => 'Oignons frits', 'quantity' => 30],
+                ]
+            ],
+            [
+                'category' => 'Burgers',
+                'name' => 'Chicken Burger',
+                'tax' => 10,
+                'ingredients' => [
+                    ['name' => 'Pain burger', 'quantity' => 1],
+                    ['name' => 'Viande Poulet', 'quantity' => 180],
+                    ['name' => 'Salade', 'quantity' => 25],
+                    ['name' => 'Cornichons', 'quantity' => 15],
+                ]
+            ],
 
-        foreach ($products as $product) {
-            if (rand(0, 1)) {
-                $randomIngredients = $ingredients->random(rand(1, 5));
+            // Frites
+            [
+                'category' => 'Frites',
+                'name' => 'Frites Classiques',
+                'price' => 3.50,
+                'tax' => 10
+            ],
+            [
+                'category' => 'Frites',
+                'name' => 'Frites Géantes',
+                'price' => 5.00,
+                'tax' => 10
+            ],
 
-                foreach ($randomIngredients as $ingredient) {
-                    $product->ingredients()->attach($ingredient->id, [
-                        'quantity' => rand(1, 20) / 10,
-                    ]);
-                }
+            // Boissons
+            [
+                'category' => 'Boissons',
+                'name' => 'Coca-Cola 33cl',
+                'price' => 2.50,
+                'tax' => 20
+            ],
+            [
+                'category' => 'Boissons',
+                'name' => 'Eau 50cl',
+                'price' => 1.80,
+                'tax' => 20
+            ],
+
+            // Sauces
+            [
+                'category' => 'Sauces',
+                'name' => 'Sauce Andalouse',
+                'price' => 0.70,
+                'tax' => 10
+            ],
+            [
+                'category' => 'Sauces',
+                'name' => 'Sauce Samouraï',
+                'price' => 0.70,
+                'tax' => 10
+            ],
+
+            // Viandes
+            [
+                'category' => 'Viandes',
+                'name' => 'Brochette de Poulet',
+                'tax' => 10,
+                'ingredients' => [
+                    ['name' => 'Viande Poulet', 'quantity' => 200]
+                ]
+            ]
+        ]);
+
+        $products->each(function ($productData) use ($categories, $ingredients) {
+            $category = $categories[$productData['category']];
+            $autoPrice = isset($productData['ingredients']);
+
+            $product = Product::create([
+                'name' => $productData['name'],
+                'category_id' => $category->id,
+                'tax' => $productData['tax'],
+                'auto_price_enabled' => $autoPrice,
+                'price' => $autoPrice ?: $productData['price']
+            ]);
+
+
+
+            if (!empty($productData['ingredients'])) {
+                $product->ingredients()->attach(collect($productData['ingredients'])->mapWithKeys(
+                    fn($ing) => [$ingredients[$ing['name']]->id => ['quantity' => $ing['quantity']]]
+                ));
             }
-        }
-
-        $orders = Order::factory(20)->create();
-
-        foreach ($orders as $order) {
-            $randomProducts = $products->random(min(rand(1, 5), $products->count()));
-
-            foreach ($randomProducts as $product) {
-                $product->orders()->attach($order->id, [
-                    'quantity' => rand(1, 20) / 10,
-                ]);
-            }
-        }
+        });
     }
 }
