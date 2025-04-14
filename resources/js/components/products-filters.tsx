b245c566ca187d757ef1c15878ce4f409486ea53
BastienVdp <bastienpp@hotmail.com>
2025-04-14 20:43:51 +0200

    fix: same

--- resources/js/components/products-filters.tsx
+++ resources/js/components/products-filters.tsx
@@ -28,64 +28,68 @@ const ProductsFilters = ({ filters, categories }: { filters: ProductsFilters; ca
     return (
         <div className="mb-4 flex items-center justify-end space-x-4">
             {/* Catégories */}
-            <Popover open={openCategory} onOpenChange={setOpenCategory}>
-                <PopoverTrigger asChild>
-                    <div>
-                        <Button variant="outline" size="sm" role="combobox">
-                            {filters.category_id ? categoryOptions.find((c) => c.id == filters.category_id)?.name : 'Toutes les catégories'}
-                            <ChevronsUpDownIcon className="ml-2 h-4 w-4 opacity-50" />
-                        </Button>
-                    </div>
-                </PopoverTrigger>
-                <PopoverContent className="w-full p-0" align="end">
-                    <Command>
-                        <CommandInput placeholder="Rechercher une catégorie..." />
-                        <CommandList>
-                            <CommandEmpty>Aucune catégorie trouvée.</CommandEmpty>
-                            <CommandGroup>
-                                {categoryOptions.map((category) => (
-                                    <CommandItem
-                                        key={category.id}
-                                        value={category.id}
-                                        onSelect={() => handleCategorySelect(category.id)} // Fermer le popover après sélection
-                                    >
-                                        {category.name}
-                                        <CheckIcon className={`ml-auto ${filters.category_id === category.id ? 'opacity-100' : 'opacity-0'}`} />
-                                    </CommandItem>
-                                ))}
-                            </CommandGroup>
-                        </CommandList>
-                    </Command>
-                </PopoverContent>
-            </Popover>
+            <div className="relative">
+                <Popover open={openCategory} onOpenChange={setOpenCategory}>
+                    <PopoverTrigger asChild>
+                        <div>
+                            <Button variant="outline" size="sm" role="combobox">
+                                {filters.category_id ? categoryOptions.find((c) => c.id == filters.category_id)?.name : 'Toutes les catégories'}
+                                <ChevronsUpDownIcon className="ml-2 h-4 w-4 opacity-50" />
+                            </Button>
+                        </div>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-full p-0" align="end">
+                        <Command>
+                            <CommandInput placeholder="Rechercher une catégorie..." />
+                            <CommandList>
+                                <CommandEmpty>Aucune catégorie trouvée.</CommandEmpty>
+                                <CommandGroup>
+                                    {categoryOptions.map((category) => (
+                                        <CommandItem
+                                            key={category.id}
+                                            value={category.id}
+                                            onSelect={() => handleCategorySelect(category.id)} // Fermer le popover après sélection
+                                        >
+                                            {category.name}
+                                            <CheckIcon className={`ml-auto ${filters.category_id === category.id ? 'opacity-100' : 'opacity-0'}`} />
+                                        </CommandItem>
+                                    ))}
+                                </CommandGroup>
+                            </CommandList>
+                        </Command>
+                    </PopoverContent>
+                </Popover>
+            </div>
 
             {/* État */}
-            <Popover open={openStatus} onOpenChange={setOpenStatus}>
-                <PopoverTrigger asChild>
-                    <div>
-                        <Button variant="outline" size="sm" role="combobox">
-                            {filters.status ? statusOptions.find((s) => s.value === filters.status)?.label : 'Tous les états'}
-                            <ChevronsUpDownIcon className="ml-2 h-4 w-4 opacity-50" />
-                        </Button>
-                    </div>
-                </PopoverTrigger>
-                <PopoverContent className="w-[180px] p-0" align="end">
-                    <Command>
-                        <CommandInput placeholder="Rechercher un état..." />
-                        <CommandList>
-                            <CommandEmpty>Aucun état trouvé.</CommandEmpty>
-                            <CommandGroup>
-                                {statusOptions.map((status) => (
-                                    <CommandItem key={status.value} value={status.value} onSelect={() => handleStatusSelect(status.value)}>
-                                        {status.label}
-                                        <CheckIcon className={`ml-auto ${filters.status === status.value ? 'opacity-100' : 'opacity-0'}`} />
-                                    </CommandItem>
-                                ))}
-                            </CommandGroup>
-                        </CommandList>
-                    </Command>
-                </PopoverContent>
-            </Popover>
+            <div className="relative">
+                <Popover open={openStatus} onOpenChange={setOpenStatus}>
+                    <PopoverTrigger asChild>
+                        <div>
+                            <Button variant="outline" size="sm" role="combobox">
+                                {filters.status ? statusOptions.find((s) => s.value === filters.status)?.label : 'Tous les états'}
+                                <ChevronsUpDownIcon className="ml-2 h-4 w-4 opacity-50" />
+                            </Button>
+                        </div>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-[180px] p-0" align="end">
+                        <Command>
+                            <CommandInput placeholder="Rechercher un état..." />
+                            <CommandList>
+                                <CommandEmpty>Aucun état trouvé.</CommandEmpty>
+                                <CommandGroup>
+                                    {statusOptions.map((status) => (
+                                        <CommandItem key={status.value} value={status.value} onSelect={() => handleStatusSelect(status.value)}>
+                                            {status.label}
+                                            <CheckIcon className={`ml-auto ${filters.status === status.value ? 'opacity-100' : 'opacity-0'}`} />
+                                        </CommandItem>
+                                    ))}
+                                </CommandGroup>
+                            </CommandList>
+                        </Command>
+                    </PopoverContent>
+                </Popover>
+            </div>
 
             {(filters.category_id || filters.status) && (
                 <TooltipProvider>
